506dd0a20559485d89169f073586dcb34bdde972
Brixn <109771702+imbriankeeley@users.noreply.github.com>
2022-08-25 15:53:29 -0400

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,45 @@
+const hourEL = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+const ampmEl = document.getElementById("ampm");
+const backgroundImage = document.getElementsByTagName("body");
+
+function updateClock(){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(h > 12) {
+        h = h - 12
+        ampm = "PM"
+    }
+
+    h = h<10 ? "0" + h : h;
+    m = m<10 ? "0" + m : m;
+    s = s<10 ? "0" + s : s;
+
+    hourEL.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl, (innerText = ampm);
+
+    setTimeout(()=>{
+    updateClock()
+    }, 1000)
+}
+
+updateClock();
+
+function updateBackground(){
+    let h = new Date().getHours()
+    if(h > 20){
+        document.body.style.backgroundImage = "https://images.unsplash.com/photo-1502481851512-e9e2529bfbf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80";
+    } else {
+        document.body.style.backgroundImage = "https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80";
+    }
+
+}
+
+updateBackground()
+
